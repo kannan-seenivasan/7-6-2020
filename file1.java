7e68b41643f642cb873c61888e6a64230e3009ad
kannan1997 <kannanseenivasan97@gmail.com>
2020-06-07 21:48:16 +0530

    first commit -changed one

---
+++ file1.java
@@ -0,0 +1,6 @@
+class kannan{
+         public static void main(string[] args){
+                  system.out.println("Welcome by kannan");
+                   system.out.println("second time edited to keep count");
+              }
+}
